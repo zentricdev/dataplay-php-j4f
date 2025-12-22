5626bd99518df2b619ac76f20399ab1ac483497a
zentricdev <zentricdev@gmail.com>
2025-12-23 00:34:48 +0100

    feat: add static throw to skynet exception

--- src/SkyNet/Exceptions/SkyNetException.php
+++ src/SkyNet/Exceptions/SkyNetException.php
@@ -4,4 +4,10 @@ declare(strict_types=1);
 
 namespace SkyNet\Exceptions;
 
-final class SkyNetException extends \Exception {}
+final class SkyNetException extends \Exception
+{
+    public static function throw(string $message): SkyNetException
+    {
+        throw new self($message);
+    }
+}
